6dbea2fb919a65daca1cce83d93083f3db9b2abb
Pia Soy <piasoy4@gmail.com>
2018-06-12 19:44:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, currentName){
 }
 
 function nowServing(katzDeliLine){
-  
+  console.log(nowServing);
  if (katzDeliLine.length === 0){
    return ('There is nobody waiting to be served!')
  }
